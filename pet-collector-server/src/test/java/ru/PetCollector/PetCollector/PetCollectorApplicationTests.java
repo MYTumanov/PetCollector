2a535b9f81a086ecb8c8d1f749d88b0b3e1268a5
Tumanov Maksim <MYTumanov@yandex.ru>
2022-12-17 21:35:42 +0300

    Добавлено простое веб приложение с аутентификацией.

---
+++ pet-collector-server/src/test/java/ru/PetCollector/PetCollector/PetCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.PetCollector.PetCollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
