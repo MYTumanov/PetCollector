9c8b7a4a0882b5f8f1a982cd8b70dbbfb8204849
WrinkleJ <WrinkleJ@github.com>
2023-01-08 23:02:14 +0300

    add linter to frontend

--- frontend/src/containers/main/index.tsx
+++ frontend/src/containers/main/index.tsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react'
 
-export function Main() {
+export const Main: React.FC = () => {
   return (
     <div>
       <ul>
         <li>Максим -2000р</li>
-        <li>Денис  +4000р</li>
-        <li>Маша   +4000р</li>
+        <li>Денис +4000р</li>
+        <li>Маша +4000р</li>
         <li></li>
       </ul>
     </div>
-  );
+  )
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,10 @@
-import { createRoot } from "react-dom/client";
-import { Main } from "./containers";
-import React from "react";
-import "./styles/main.css";
+import { createRoot } from 'react-dom/client'
+import { Main } from './containers'
+import React from 'react'
+import './styles/main.css'
 
-document.addEventListener("DOMContentLoaded", () => {
-  const rootNode = document.getElementById("root");
-  const root = createRoot(rootNode);
-  root.render(<Main></Main>);
-});
+document.addEventListener('DOMContentLoaded', () => {
+  const rootNode = document.getElementById('root') as HTMLElement
+  const root = createRoot(rootNode)
+  root.render(<Main></Main>)
+})
